4c5681225c47e2adb0d83b0049bd645979b55f14
Benjamen King <brk.3rdof3kings@gmail.com>
2017-12-17 03:32:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -137,7 +137,8 @@ function moveDodger(e) {
     e.preventDefault();
     e.stopPropagation();
     moveDodgerLeft();
-  } else if (e.which === RIGHT_ARROW) {
+  }
+  if (e.which === RIGHT_ARROW) {
     e.preventDefault();
     e.stopPropagation();
     moveDodgerRight();
